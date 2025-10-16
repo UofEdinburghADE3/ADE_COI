2c5c6bc45d3ec34d3435088e1e82af03f8a7ae4e
Áine O'Toole <aine.otoole@ed.ac.uk>
2025-10-16 11:39:55 +0100

    Update adecoi_runner.sh

--- adecoi/scripts/adecoi_runner.sh
+++ adecoi/scripts/adecoi_runner.sh
@@ -1,19 +1,19 @@
 
 
-snakemake --snakefile /localdisk/home/s1680070/repositories/ADE_COI/adecoi/scripts/Snakefile \
-            --configfile /localdisk/home/s1680070/repositories/ADE_COI/adecoi/data/config.yaml \
+snakemake --snakefile /home/s1680070/repositories/ADE_COI/adecoi/scripts/Snakefile \
+            --configfile /home/s1680070/ADE_2025/updated_config.yaml \
             --cores 10 \
             --forceall \
             --rerun-incomplete \
             --nolock
 
-snakemake --snakefile /localdisk/home/s1680070/repositories/ADE_COI/adecoi/scripts/run_analysis.smk \
-            --configfile /localdisk/home/s1680070/ADE_2022/combined_analysis/analysis_config.yaml \
+snakemake --snakefile /home/s1680070/repositories/ADE_COI/adecoi/scripts/run_analysis.smk \
+            --configfile /home/s1680070/ADE_2025/analysis/analysis_config.yaml \
             --cores 10 \
             --forceall \
             --rerun-incomplete \
             --nolock
 
-rm /localdisk/home/s1680070/repositories/ADE_COI/adecoi/ADE_2022/barcode*/consensus_sequences/*.txt
+# rm /home/s1680070/repositories/ADE_COI/adecoi/ADE_2022/barcode*/consensus_sequences/*.txt
 
-python adecoi/scripts/index_report.py #need a barcode name map file
+# python adecoi/scripts/index_report.py #need a barcode name map file
